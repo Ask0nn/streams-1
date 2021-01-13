c733e496e0d82d3263e08fe0cd558d1ea73975b9
Ask0nn <Askonzir@gmail.com>
2021-01-14 01:46:34 +0300

    Выполнение задания

--- src/com/ask0n/Main.java
+++ src/com/ask0n/Main.java
@@ -1,8 +1,17 @@
 package com.ask0n;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.stream()
+                .filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
     }
 }
